8dc5635f8d578a34cd072d03147ec514940965a7
MatveyGameRus <99652168+MatveyGameRus@users.noreply.github.com>
2022-02-14 17:44:12 +0700

    Add files via upload

---
+++ s.py
@@ -0,0 +1,11 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print ("Hello GitHub! ")
+Hello GitHub! 
+import sleep
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    import sleep
+ModuleNotFoundError: No module named 'sleep'
+import time
+time.sleep(2)
